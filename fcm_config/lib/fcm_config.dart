c2633f7062e8c157408ada0dd3b308d00a2192b3
Mohamed Dawood <mo.ah.dawood@gmail.com>
2021-12-03 13:52:59 +0200

    ....

--- fcm_config/lib/fcm_config.dart
+++ fcm_config/lib/fcm_config.dart
@@ -1,11 +1,9 @@
 export 'package:firebase_messaging/firebase_messaging.dart';
-export 'package:flutter_local_notifications/flutter_local_notifications.dart'
-    if (dart.library.html) '';
+export 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 export './src/click_stream_subscription.dart';
 export './src/fcm_extension.dart';
 export './src/fcm_notification_click_listener.dart';
 export './src/fcm_notification_listener.dart';
-export './src/io/fcm_config.dart'
-    if (dart.library.html) 'src/web/fcm_config.dart';
+export './src/io/fcm_config.dart' if (dart.library.html) 'src/web/fcm_config.dart';
 export 'src/details.dart';
